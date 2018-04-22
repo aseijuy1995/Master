c3f24580b75ff31ace6bdd4f8742d5ecab8a2971
aseijuy1995 <b0972911675@gmail.com>
2018-04-22 15:12:44 +0800

    ArticlrFragment

--- app/src/main/java/nom/cp101/master/master/ArticleFragment.java
+++ app/src/main/java/nom/cp101/master/master/ArticleFragment.java
@@ -4,18 +4,32 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 //文章frag
 public class ArticleFragment extends Fragment {
+    RecyclerView rv;
+    View view;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.article_frag, container, false);
+        view = inflater.inflate(R.layout.article_frag, container, false);
+        //取得RecyclerView並接上ArticleAdapter
+        setRecyclerView();
+        return view;
+    }
+
+    //取得RecyclerView並接上ArticleAdapter
+    private void setRecyclerView() {
+        rv = (RecyclerView) view.findViewById(R.id.rv_article);
+        rv.setLayoutManager(new LinearLayoutManager(getActivity()));
+        rv.setAdapter(new ArticleAdapter(getActivity(), getFragmentManager()));
     }
 
 }
