c73e98d3bc19e90e0d414ce384196cd4fdb0e5bb
aseijuy1995 <b0972911675@gmail.com>
2018-04-25 14:26:02 +0800

    test

--- app/src/main/java/nom/cp101/master/master/ArticleFragment.java
+++ app/src/main/java/nom/cp101/master/master/ArticleFragment.java
@@ -10,10 +10,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 //文章frag
 public class ArticleFragment extends Fragment {
     RecyclerView rv;
     View view;
+    List<ProjectData> projectList;
 
     @Nullable
     @Override
@@ -22,15 +26,33 @@ public class ArticleFragment extends Fragment {
         view = inflater.inflate(R.layout.article_frag, container, false);
         //取得RecyclerView並接上ArticleAdapter
         setRecyclerView();
+
         return view;
     }
+
+
 
     //取得RecyclerView並接上ArticleAdapter
     private void setRecyclerView() {
         rv = (RecyclerView) view.findViewById(R.id.rv_article);
         rv.setLayoutManager(new LinearLayoutManager(getActivity()));
-        rv.setAdapter(new ArticleAdapter(getActivity(), getFragmentManager()));
+        rv.setAdapter(new ArticleAdapter(getActivity(), getFragmentManager(), getProjectData()));
     }
 
+    //載入專業類別的數據
+    private List<ProjectData> getProjectData() {
+        projectList=new ArrayList<>();
+
+        projectList.add(new ProjectData(android.R.drawable.sym_action_email, "1"));
+        projectList.add(new ProjectData(android.R.drawable.sym_action_email, "2"));
+        projectList.add(new ProjectData(android.R.drawable.sym_action_email, "3"));
+        projectList.add(new ProjectData(android.R.drawable.sym_action_email, "4"));
+        projectList.add(new ProjectData(android.R.drawable.sym_action_email, "5"));
+        projectList.add(new ProjectData(android.R.drawable.sym_action_email, "6"));
+        projectList.add(new ProjectData(android.R.drawable.sym_action_email, "7"));
+        projectList.add(new ProjectData(android.R.drawable.sym_action_email, "8"));
+
+        return projectList;
+    }
 }
 
